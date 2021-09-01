e84856caea8c8ca588cd618bad05446060424e2c
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-02 02:36:54 +0430

    a new url for register a user added

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('', AccountView.as_view(), name='account'),
     path('logout', logOutHandler, name='logout'),
