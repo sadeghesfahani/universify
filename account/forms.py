b71fe02624190e50650963c247b22fad0a36b2a2
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-04 01:30:47 +0430

    Register form completed

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth import authenticate
 
+from account.models import User, CustomUserManager, Faculty, Department, Position
+
 
 class LoginForm(forms.Form):
     identifier = forms.CharField(max_length=15, label='شناسه کاربری')
@@ -21,3 +23,29 @@ class LoginForm(forms.Form):
 class RegisterForm(forms.Form):
     first_name = forms.CharField(max_length=60, label='نام')
     last_name = forms.CharField(max_length=100, label='نام خانوادگی')
+    email = forms.EmailField(label='ایمیل')
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label='رمز عبور')
+    confirm_password = forms.CharField(max_length=20, widget=forms.PasswordInput, label='تکرار رمز عبور')
+
+    def matchPass(self):
+        return self.cleaned_data['password'] == self.cleaned_data['confirm_password']
+
+    def is_valid(self):
+        super(RegisterForm, self).is_valid()
+        return self.matchPass()
+
+    def get_position(self):
+        try:
+            faculty = Faculty.objects.get(name=self.data['faculty'])
+            department = Department.objects.get(name=self.data['department'], faculty_id=faculty.id)
+            position = Position.objects.get(name=self.data['position'], department_id=department.id)
+        except:
+            position = None
+        return position
+
+    def addUser(self):
+        user = User(first_name=self.cleaned_data['first_name'], last_name=self.cleaned_data['last_name'],
+                    position=self.get_position(),email=self.cleaned_data['email'])
+
+        user.set_password(self.cleaned_data['password'])
+        user.save()
