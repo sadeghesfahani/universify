3c9c60f32dce7ce49990618315bfb3a915643343
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-03 17:27:26 +0430

    2 urls added: one return department list by their faculty-name and another return positions by their department-name

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from .views import *
 
 app_name = 'account'
@@ -7,5 +9,6 @@ urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('', AccountView.as_view(), name='account'),
     path('logout', logOutHandler, name='logout'),
-
+    path('department-list/<str:faculty>', DepartmentList.as_view(), name='department-list'),
+    path('position-list/<str:department>', PositionList.as_view(), name='position-list')
 ]
