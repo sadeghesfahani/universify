f207cf5b7cfd1588d399cdfcb91a844cf028c599
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2021-09-03 17:25:48 +0430

    rest_framework added to INSTALLED_APPS

--- university_controller/settings.py
+++ university_controller/settings.py
@@ -35,9 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'account',
     'library',
-    'curriculum'
+    'curriculum',
 ]
 
 MIDDLEWARE = [
